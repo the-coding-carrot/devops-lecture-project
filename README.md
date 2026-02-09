6270f977a7edf8eb1eb8ade15c6a88e2f8c3ad9a
Maxim Tausch <maxim.tausch2004@gmail.com>
2026-02-09 17:25:59 +0100

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# devops-lecture-project-2025
+
+Project for the lecture "Einführung in DevOps, Continuous Delivery Tools und Mindset"
---
+++ cmd/main.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var secretKey = []byte("secret-key")
+
+type Product struct {
+	ID    int     `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+// Static data for three products
+var products = []Product{
+	{ID: 1, Name: "Product 1", Price: 19.99},
+	{ID: 2, Name: "Product 2", Price: 29.99},
+	{ID: 3, Name: "Product 3", Price: 39.99},
+}
+
+func main() {
+	mux := http.NewServeMux()
+	// Auth Service
+	mux.HandleFunc("/auth/login", authLoginHandler)
+	mux.HandleFunc("/auth/logout", authLogoutHandler)
+	// Product Service
+	mux.HandleFunc("/products", productListHandler)
+	mux.HandleFunc("/products/{id}", productDetailHandler)
+	// Checkout Service
+	mux.HandleFunc("/checkout/placeorder", checkoutPlaceOrderHandler)
+	port := 8080
+	log.Printf("Server is running on port %d...\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), mux))
+}
+
+func authLoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+	// For simplicity, we'll use a hardcoded username and password
+	// This should be replaced with a proper authentication mechanism
+	if username == "user" && password == "pass" {
+		token, err := createToken(username)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte(`{"error": "Error generating the token"}`))
+			return
+		}
+		w.Write([]byte(fmt.Sprintf(`{"token": "%s"}`, token)))
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(`{"error": "Invalid credentials"}`))
+	}
+}
+
+func authLogoutHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	// In this simple example, we'll just return a success message
+	w.Write([]byte(`{"message": "Logout successful"}`))
+}
+
+func productListHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	response, err := json.Marshal(products)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"error": "Internal Server Error"}`))
+		return
+	}
+	w.Write(response)
+}
+
+func productDetailHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	idStr := r.PathValue("id")
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, `{"error":"Product ID has wrong format"}`, http.StatusBadRequest)
+		return
+	}
+
+	product := findProductByID(products, id)
+	if product == nil {
+		http.Error(w, `{"error":"Product not found"}`, http.StatusNotFound)
+		return
+	}
+
+	resp, err := json.Marshal(product)
+	if err != nil {
+		http.Error(w, `{"error":"Internal Server Error"}`, http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(resp)
+}
+
+func checkoutPlaceOrderHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(`{"error":"Missing Authorization header"}`))
+		return
+	}
+
+	const bearerPrefix = "Bearer "
+	if !strings.HasPrefix(authHeader, bearerPrefix) {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(`{"error":"Authorization header must use Bearer scheme"}`))
+		return
+	}
+
+	tokenString := strings.TrimPrefix(authHeader, bearerPrefix)
+
+	if !verifyToken(tokenString) {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(`{"error":"Invalid token"}`))
+		return
+	}
+
+	w.Write([]byte(`{"message":"Order placed successfully"}`))
+}
+
+func createToken(username string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"username": username,
+			"exp":      time.Now().Add(time.Hour * 24).Unix(),
+		})
+	tokenString, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
+
+func verifyToken(tokenString string) bool {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method")
+		}
+		return secretKey, nil
+	})
+
+	return err == nil && token.Valid
+}
+
+func findProductByID(products []Product, id int) *Product {
+	for _, product := range products {
+		if product.ID == id {
+			return &product
+		}
+	}
+	return nil
+}
