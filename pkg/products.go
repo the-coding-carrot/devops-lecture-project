47e2e82c35076ac5792dcd26c305a755eeb59b1d
Maxim Tausch <maxim.tausch2004@gmail.com>
2026-02-16 18:07:58 +0100

    refactor: refactored the pkg package further into a products and token script

---
+++ pkg/products.go
@@ -0,0 +1,18 @@
+package pkg
+
+var secretKey = []byte("secret-key")
+
+type Product struct {
+	ID    int     `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+func FindProductByID(products []Product, id int) *Product {
+	for _, product := range products {
+		if product.ID == id {
+			return &product
+		}
+	}
+	return nil
+}
