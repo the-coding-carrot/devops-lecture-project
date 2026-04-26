da2eb9f74f338f3f9a95b1bd8564aef294c1db5e
Maxim Tausch <maxim.tausch2004@gmail.com>
2026-04-26 16:52:53 +0200

    ci: adjusted ymls for azure and added tf

---
+++ tf/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+    description = "location of the azure resource"
+    type = string
+    default = "westeurope"
+}
+
+variable "resource_group_name" {
+    description = "Name of the resource group"
+    type = string
+    default = "web-shop"
+}
