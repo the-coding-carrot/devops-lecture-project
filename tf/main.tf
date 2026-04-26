6e0ef73ab4a929f38d18d2e2f992d9e0ff3173d7
Maxim Tausch <maxim.tausch2004@gmail.com>
2026-04-26 18:10:28 +0200

    ci: fixed lgtm

--- tf/main.tf
+++ tf/main.tf
@@ -21,10 +21,12 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix = "aksproject"
+  oidc_issuer_enabled = true
+  workload_identity_enabled = true
 
   default_node_pool {
     name = "default"
-    node_count = 1
+    node_count = 2
     vm_size = "Standard_B2ps_v2"
   }
 
